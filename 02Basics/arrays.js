e47099d384db59c13b812766d783cb94bf970b56
Shail Patel <shailpatel1996@gmail.com>
2019-05-06 19:19:15 -0400

    advance arrays

--- 02Basics/arrays.js
+++ 02Basics/arrays.js
@@ -1,7 +1,29 @@
-const Mynames = ['Shail', 'Shaeeel', 'Saeeel','Riya', 'Rieyaaa', 'Rea']
+const Mynames = ['Shail', 'Shaeeel', 'Saeeel', 'Riya', 'Rieyaaa', 'Rea']
 
 console.log(Mynames);
 console.log(Mynames[1]);
 console.log(Mynames[Mynames.length - 1]);
 
 console.log(`I have ${Mynames.length} names`);
+
+Mynames[Mynames.length] = 'Riyaaaaaaa'
+
+console.log(Mynames);
+
+// Start
+Mynames.shift(); // removes the first element
+
+Mynames.unshift('New Item'); // adds new item at zero position
+
+
+// End
+Mynames.pop(); // removed the last element
+
+Mynames.push('Riyeeaee'); // add the element at last
+
+console.log(Mynames);
+
+
+// Middle
+
+Mynames.splice(2,1,'testing'); // removes the element at pos 2 , take away 1 element and add string
