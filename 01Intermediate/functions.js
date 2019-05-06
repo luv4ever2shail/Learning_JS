864294a5366718aa076f9780650cdf1fb322f07f
Shail Patel <shailpatel1996@gmail.com>
2019-05-06 19:58:11 -0400

    Added functions

---
+++ 01Intermediate/functions.js
@@ -0,0 +1,25 @@
+let sayHello = function (name) {
+    console.log('Greeting for user');
+    console.log(`Hey ${name}`);
+}
+
+sayHello('Riya')
+
+function total(num1, num2) {
+    return num1 + num2
+}
+
+let result = total(2, 4)
+
+console.log(result);
+
+
+console.log(total(2, 4));
+
+
+let guestUser = function (name = 'NoName', count =0) {
+    return 'Hello ' + name + ' and your course count is : ' + count
+    
+}
+
+console.log(guestUser('Shail', 1));
